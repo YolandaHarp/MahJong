2cd1f5d2b1a060a1a6637142bfcb7bb45f49efcc
tianpeiq <163368922+tianpeiq@users.noreply.github.com>
2024-05-21 21:46:53 +0800

    Add files via upload
    draw dot cards

---
+++ src/dot/MJ6.java
@@ -0,0 +1,19 @@
+package screen.cardsScreen.Dot;
+
+import javafx.scene.canvas.GraphicsContext;
+import screen.cardsScreen.Drawable;
+
+import static screen.cardsScreen.CardScreen.green;
+import static screen.cardsScreen.CardScreen.red;
+import static screen.cardsScreen.Dot.DotScreen.draw4Dot;
+import static screen.cardsScreen.Dot.DotScreen.drawDot;
+
+public class MJ6 implements Drawable {
+    @Override
+    public void draw(GraphicsContext g, int x, int y, double a) {
+        draw4Dot(g,x,y,a,red);
+        drawDot(g, x, y, 472, 461, 20, a, green, 5);
+        drawDot(g, x, y, 490, 467, 20, a, green, 5);
+        drawDot(g, x, y, 507, 473, 20, a, green, 5);
+    }
+}
---
+++ src/dot/MJ7.java
@@ -0,0 +1,16 @@
+package screen.cardsScreen.Dot;
+
+import javafx.scene.canvas.GraphicsContext;
+import screen.cardsScreen.Drawable;
+
+import javafx.scene.paint.Color;
+
+import static screen.cardsScreen.Dot.DotScreen.draw4Dot;
+
+public class MJ7 implements Drawable {
+    @Override
+    public void draw(GraphicsContext g, int x, int y, double a) {
+        draw4Dot(g,x, (int) (y+5*a),a,Color.BLACK);
+        draw4Dot(g,x, (int) (y-37*a),a,Color.BLACK);
+    }
+}
