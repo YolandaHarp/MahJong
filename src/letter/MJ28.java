011d8e414123607b283c25d97acba394af398ac3
tianpeiq <163368922+tianpeiq@users.noreply.github.com>
2024-05-22 10:07:24 +0800

    Add files via upload
    draw letter cards

---
+++ src/letter/MJ28.java
@@ -0,0 +1,21 @@
+package screen.cardsScreen.Letter;
+
+import javafx.scene.canvas.GraphicsContext;
+import screen.cardsScreen.Drawable;
+
+import javafx.scene.paint.Color;
+
+import static screen.cardsScreen.CardScreen.*;
+
+public class MJ28 implements Drawable {
+    @Override
+    public void draw(GraphicsContext g, int x, int y, double a) {
+        int[] x_coords = new int[]{-7, -4, -11, -18, -11, -4, 6, 12, 8, -3, -3, 0, -4, -5, -5, -5, -6, -5, -4, -4, -7};
+        int[] y_coords = new int[]{0, -15, -13, -13, -13, -15, -18, -17, -18, -16, -24, -25, -26, -29, -35, -29, -30, -31, -26, -17, 0};
+        drawPg(g, x_coords, y_coords, x, y, 0.9 * a, white, Color.BLACK, 5f);
+
+        int[] x_coords2 = new int[]{-6, -4, -6, -14, -6, 9, -6, -3, -15, -3, 8, -3, 3, 5, -7, -10, -19, -22, -17, -22, -23, -19, -22, -19, -10, -15, -10, -7, 5, 14, 20, 23, 20, 20, 20, 16, 8, 5};
+        int[] y_coords2 = new int[]{15, 29, 15, 18, 15, 12, 15, 6, 10, 6, 5, 6, -4, -3, 0, 1, 4, 6, 29, 6, 4, 4, 6, 4, 1, -5, 1, 0, -2, -2, 1, 2, 3, 1, 2, 21, 29, 22};
+        drawPg(g, x_coords2, y_coords2, x, y, 1 * a, white, Color.BLACK, 5f);
+    }
+}
---
+++ src/letter/MJ31.java
@@ -0,0 +1,19 @@
+package screen.cardsScreen.Letter;
+
+import javafx.scene.canvas.GraphicsContext;
+import screen.cardsScreen.Drawable;
+
+import static screen.cardsScreen.CardScreen.*;
+
+public class MJ31 implements Drawable {
+    @Override
+    public void draw(GraphicsContext g, int x, int y, double a) {
+        int[] x_coords = new int[]{-21,-19,0,8,12,19,18,14,9,8,5,-7,-10,-13,-14,-21};
+        int[] y_coords = new int[]{-27,-26,-30,-32,-32,-27,-27,-21,-17,-19,-19,-17,-16,-16,-15,-27};
+        drawPg(g, x_coords, y_coords, x, y+15, a, white, red, 5f);
+
+        int[] x_coords2 = new int[]{0,-1,-1,-5,-2,4,1,1,0};
+        int[] y_coords2 = new int[]{38,35,-22,-22,-25,-22,-22,35,38};
+        drawPg(g, x_coords2, y_coords2, x-3, y-8, a, white, red, 4.4f);
+    }
+}
