90df5065f71ac42b3141c04f494cab68c3ff8051
SYB1103 <73373623+SYB1103@users.noreply.github.com>
2024-05-28 23:08:18 +0800

    Connect the front-end to the back-end

--- GUI/Main2.java
+++ GUI/Main2.java
@@ -1,48 +1,42 @@
-package fx;
+package GUI;
 
+import game.Mahjong;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import screen.cardsScreen.Bam.BamScreen;
-import javafx.scene.canvas.Canvas;
-import screen.cardsScreen.CardScreen;
 
 
-import java.awt.*;
 import java.io.IOException;
-import java.util.ArrayList;
 
 
 public class Main2 extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("New.FXML"));
+        new Thread(() -> {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("New.fxml"));
 
-        // 获取TabPane
-
-        Scene scene = new Scene(root, 1280, 830);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        /*new Thread(() ->{
-            System.out.println(ActionButtonsPane.getButton().getChoice());
-            System.out.println(1);
-        }).start();*/
-
-
-
+                // Ensure the UI update is run on the JavaFX Application Thread
+                Platform.runLater(() -> {
+                    Scene scene = new Scene(root);
+                    primaryStage.setScene(scene);
+                    primaryStage.setOnCloseRequest(event -> System.exit(0));
+                    primaryStage.show();
+                });
+                Mahjong.getMJ().startGame();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
     
     public static void main(String[] args) {
-        launch(args);
 
+
+        launch(args);
     }
 
 }
