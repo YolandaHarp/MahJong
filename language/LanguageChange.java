1ab05d3e6e8352e77531d5a03519b5869e4e4e70
Yolanda_Harp <147145676+YolandaHarp@users.noreply.github.com>
2024-06-03 23:47:49 +0800

    implemention of multi-language

---
+++ language/LanguageChange.java
@@ -0,0 +1,41 @@
+package language;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+public class LanguageChange {
+    private int languageCode=0;
+    private final int LANGUAGE_TOTAL = 2;
+    private static LanguageChange language=new LanguageChange();
+    private LanguageChange(){}
+    public static LanguageChange getLanguage(){
+        return language;
+    }
+    public void languageUp() {
+        languageCode++;
+        if(languageCode== LANGUAGE_TOTAL){
+            languageCode=0;
+        }
+    }
+    public void languageDown() {
+        languageCode--;
+        if(languageCode<0){
+            languageCode= LANGUAGE_TOTAL -1;
+        }
+    }
+    public String getString(String key){
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getResource("language_" + languageCode + ".properties").openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return new String(properties.getProperty(key).toString().getBytes(),"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
