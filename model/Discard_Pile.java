5af16c6fdad2bea5b453f42552eb060cb6d0b43a
SYB1103 <73373623+SYB1103@users.noreply.github.com>
2024-06-05 10:03:37 +0800

    Refactoring code

--- model/Discard_Pile.java
+++ model/Discard_Pile.java
@@ -2,13 +2,11 @@ package model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
 
 public class Discard_Pile implements Serializable {
-    String[] table = new String[]{"一筒","二筒","三筒","四筒","五筒","六筒","七筒","八筒","九筒",
-            "一条","二条","三条","四条","五条","六条","七条","八条","九条",
-            "一万","二万","三万","四万","五万","六万","七万","八万","九万","东","南","西",
-            "北","中","発","白","混"};
-    private ArrayList<Integer> cards = new ArrayList<Integer>();
+    private Stack<Integer> cards = new Stack<Integer>();
     private static Discard_Pile discard=new Discard_Pile();
 
     private Discard_Pile() {
@@ -18,27 +16,21 @@ public class Discard_Pile implements Serializable {
     }
 
     void add(int card){
-        cards.add(card);
+        cards.push(card);
     }
     void remove(){
-        cards.remove(cards.size() - 1);
+        cards.pop();
     }
-    int getLast(){
-        return cards.get(cards.size() - 1);
+    public int getLast(){
+        return cards.peek();
     }
     public void clear(){
-        cards = new ArrayList<Integer>();
+        cards = new Stack<Integer>();
     }
-    public ArrayList<Integer> show(){
-        return new ArrayList<Integer>(cards);
+    public Iterator<Integer> show(){
+        return cards.iterator();
     }
-    public void wenzi(){
-        for(int i:cards){
-            System.out.print(table[i]+" ");
-        }
-        System.out.println();
-    }
-    public void setDiscard(ArrayList<Integer> c){
-        cards=c;
+    public void setDiscard(Discard_Pile d){
+        discard=d;
     }
 }
